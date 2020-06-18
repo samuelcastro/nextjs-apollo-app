0aaf0c654167f4e6875cba33ed07461508a3841c
bengrunfeld <benjamin.grunfeld@gmail.com>
2020-06-18 17:15:00 +0300

    Implement full stack graphql app

---
+++ components/BookInfo/BookInfo.jsx
@@ -0,0 +1,56 @@
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const GET_BOOK_DETAILS = gql`
+  query {
+    book {
+      name
+      author
+    }
+  }
+`;
+
+const SET_BOOK_DETAILS = gql`
+  mutation UpdateBook($name: String!, $author: String!) {
+    updateBook(name: $name, author: $author) {
+      name
+      author
+    }
+  }
+`;
+
+const BookInfo = () => {
+  const { loading, error, data } = useQuery(GET_BOOK_DETAILS);
+
+  const updateCache = (cache, { data: { updateBook } }) => {
+    const existingBook = cache.readQuery({
+      query: GET_BOOK_DETAILS,
+    });
+    cache.writeQuery({
+      query: GET_BOOK_DETAILS,
+      data: { book: updateBook },
+    });
+  };
+
+  const [updateBook] = useMutation(SET_BOOK_DETAILS, { update: updateCache });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const updateBookDetails = () => {
+    updateBook({
+      variables: { name: "A Spicy Sausage", author: "Anton the Butcher" },
+    });
+  };
+
+  return (
+    <div>
+      <p>
+        {data.book.name} - {data.book.author}
+      </p>
+      <button onClick={updateBookDetails}>Update Book</button>
+    </div>
+  );
+};
+
+export default BookInfo;
---
+++ pages/api/graphql-data.js
@@ -0,0 +1,45 @@
+import { ApolloServer, gql } from "apollo-server-micro";
+
+let book = {
+  name: "The Hungarian Sausage",
+  author: "Ben Grunfeld",
+};
+
+const typeDefs = gql`
+  type Book {
+    name: String
+    author: String
+  }
+  type Query {
+    book: Book
+  }
+  type Mutation {
+    updateBook(name: String!, author: String!): Book
+  }
+`;
+
+const resolvers = {
+  Query: {
+    book: () => book,
+  },
+
+  Mutation: {
+    updateBook: (root, args) => {
+      book.name = args.name;
+      book.author = args.author;
+      return book;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const handler = server.createHandler({ path: "/api/graphql-data" });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
